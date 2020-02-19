a83d13b28112e2145380fef6e589bced76242775
zhangshuai <zhangshuai4181@126.com>
2020-02-19 11:36:09 +0800

    20200219

---
+++ lcm_gen.sh
@@ -0,0 +1,6 @@
+lcm-gen -x vehicle_info.lcm;
+lcm-gen -x location.lcm;
+lcm-gen -x worldmodle_vehicle.lcm;
+lcm-gen -x worldmodle_vehicles.lcm;
+lcm-gen -x expireacc.lcm
+lcm-gen -x platoon_mananger.lcm
