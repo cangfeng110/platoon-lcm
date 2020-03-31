d291a0cbca8724ecfa1056547bb3531c7d230fd9
zhangshuai <zhangshuai4181@126.com>
2020-03-31 16:07:33 +0800

    add tofusioninfo,fix protodata

--- lcm_gen.sh
+++ lcm_gen.sh
@@ -4,3 +4,5 @@ lcm-gen -x worldmodle_vehicle.lcm;
 lcm-gen -x worldmodle_vehicles.lcm;
 lcm-gen -x expireacc.lcm
 lcm-gen -x platoon_mananger.lcm
+lcm-gen -x front_vehicle_info.lcm 
+lcm-gen -x fms_info.lcm
